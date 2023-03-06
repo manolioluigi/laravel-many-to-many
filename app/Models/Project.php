ac16748caa5f1eaa036822f5392f9f6fc9fe216d
manolioluigi <luigi.manolio1999@gmail.com>
2023-03-06 18:25:06 +0100

    collegamento models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use App\Models\Type;
+use App\Models\Technology;
 
 class Project extends Model
 {
-    protected $fillable = ['title', 'content', 'slug', 'type_id'];
+    protected $fillable = ['title', 'content', 'slug', 'type_id', 'technologies_id'];
     use HasFactory;
 
     public static function generateSlug($title){
@@ -18,5 +19,9 @@ class Project extends Model
 
     public function type(){
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies(){
+        return $this->belongsToMany(Technology::class);
     }
 }
